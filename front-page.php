f0d46d158205fe62760eb8860293359a715a42f2
Simantob <e2295345@cmaisonneuve.qc.ca>
2023-03-08 08:35:07 -0500

    C11 modification template-parts

--- front-page.php
+++ front-page.php
@@ -12,11 +12,12 @@ get_header() ?>
             while (have_posts()) : the_post();
                 //the_title('<h1>','</h1>');
                 //the_permalink(); ?>
-                <?php get_template_part('template-parts/categorie', 'note-wp'); ?>
-                <article>
-                    <h3><a href="<?php the_permalink(); ?>"><?= get_the_title();  ?> </a></h3>
-                    <p><?php  echo wp_trim_words(get_the_excerpt(), 4); ?></p>
-                </article>
+                <?php 
+                $ma_categorie ="note-wp";
+                if (in_category('galery')){$ma_categorie = "galerie";}
+                get_template_part('template-parts/categorie', $ma_categorie);
+                 ?>
+                
             <?php endwhile; ?>
         <?php endif; ?>   
     </section>
--- header.php
+++ header.php
@@ -23,25 +23,9 @@
         <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
         <h2><?php  bloginfo('description'); ?></h2>
     </header>
+    
+    <?php get_template_part("template-parts/aside"); ?>
 
-    <aside class="site__aside"> <!-- va s'executer ds chacune des page de notre wp, car c'est dans le header -->
-        
-        <h3>Menu secondaire</h3>
-        <?php 
-        $category = get_queried_object(); /* recup l'URL pour en faire un objet */
-        if(isset($category))
-        {
-            $lemenu = $category->slug;
-        }
-        else /* permet d'avoir une valeur par defaut pour le menu */
-        {
-            $lemenu = "note-wp";
-        }
-        /* permet d'afficher le menu des notes de cours */
-        wp_nav_menu(array
-        (
-            "menu" => $lemenu,
-            "container" => "nav"
-        )) ?>
-    </aside>
+
+    
     

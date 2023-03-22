941522484114efacb63d2c9ccb239d5612ef8066
Simantob <e2295345@cmaisonneuve.qc.ca>
2023-03-22 11:24:01 -0400

    C15 rajout du nom des enseignants et domaine pour tous les cours

--- single.php
+++ single.php
@@ -9,6 +9,7 @@ get_header() ?>
 if (have_posts()):
    while(have_posts()): the_post();
         the_title('<h1>','</h1>');
+        the_field("enseignant"); 
         the_content();  
     endwhile;    
 endif;

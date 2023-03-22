f0103b470265079a8331967db934acc9bffc782d
Simantob <e2295345@cmaisonneuve.qc.ca>
2023-03-22 11:45:09 -0400

    C15 rajout dans single.php des infos, reste la mise en page à faire

--- single.php
+++ single.php
@@ -9,7 +9,10 @@ get_header() ?>
 if (have_posts()):
    while(have_posts()): the_post();
         the_title('<h1>','</h1>');
-        the_field("enseignant"); 
+        the_field("enseignant");
+        the_field("domaine"); 
+        the_field("horaire"); 
+        the_field("classe"); 
         the_content();  
     endwhile;    
 endif;
